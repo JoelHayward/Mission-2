5231f309bbd62f05ac23fb50fde138f084afa893
Joel <joel.m.hayward@gmail.com>
2022-06-07 11:23:35 +1200

    added info upload to object array

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 //PRELOADER
+setTimeout(() => {
+  document.getElementById("preloader").style.display = "none";
+}, "10000");
 
-setTimeout(() => {document.getElementById("preloader").style.display = "none"}, '10000')
-setTimeout();
+//Upload perosns information data to an object
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", addInfo);
+});
+
+const theirInformation = [];
+const addInfo = (ev) => {
+  ev.preventDefault();
+  const info = {
+    id: Date.now(),
+    name: document.getElementById("their_name").value,
+    email: document.getElementById("their_email").value,
+    number: document.getElementById("their_number").value,
+    rego: document.getElementById("their_rego").value,
+    license: document.getElementById("their_license").value,
+    licenseImg: document.getElementById("license_img").value,
+  };
+  theirInformation.push(info);
+  document.querySelector("form").reset();
+  console.log(theirInformation);
+};
 
 //Upload image for their drivers license in display_img1
 const imgLicense = document.querySelector("imgLicense");
 const upload_image = "";
 
-imgLicense.addEventListener("change", function () {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    upload_image = reader.result;
-    document.querySelector(
-      ".display_img1"
-    ).style.backgroundImage = `url(${upload_image})`;
-  });
-  reader.readAsDataURL(this.file[0]);
-});
+
