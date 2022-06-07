367781106156290d246b6ed4f07e3856bc6e856c
Joel <joel.m.hayward@gmail.com>
2022-06-08 11:39:08 +1200

    changed css, can open window email but does not load content to body

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //PRELOADER
 setTimeout(() => {
   document.getElementById("preloader").style.display = "none";
@@ -6,27 +8,36 @@ setTimeout(() => {
 //Upload perosns information data to an object
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btn").addEventListener("click", addInfo);
+  document.getElementById("send").addEventListener("click", sendInfo);
 });
 
 const theirInformation = [];
+const d = new Date();
 const addInfo = (ev) => {
   ev.preventDefault();
   const info = {
-    id: Date.now(),
+    date: (document.getElementById("newDate").innerHTML = d),
     name: document.getElementById("their_name").value,
     email: document.getElementById("their_email").value,
     number: document.getElementById("their_number").value,
     rego: document.getElementById("their_rego").value,
     license: document.getElementById("their_license").value,
     licenseImg: document.getElementById("license_img").value,
+    licenseImg: document.getElementById("first_img").value,
+    licenseImg: document.getElementById("second_img").value,
   };
   theirInformation.push(info);
   document.querySelector("form").reset();
   console.log(theirInformation);
 };
 
-//Upload image for their drivers license in display_img1
-const imgLicense = document.querySelector("imgLicense");
-const upload_image = "";
+const sendInfo = (ev) => {
+  ev.preventDefault();
+  window.open(
+    `mailto:test@example.com?subject=${'Car Accident'}&body=`+ encodeURIComponent(theirInformation));
 
+};
 
+// //Upload image for their drivers license in display_img1
+// const imgLicense = document.querySelector("imgLicense");
+// const upload_image = "";
